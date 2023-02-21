431190a1d6e4466d38812cee6bf2d14560312592
Ahmed hossam <you@example.com>
2023-02-21 18:08:30 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const socket = require('socket.io');
+const http = require('http');
+
+
+
+//App setup :D
+const app = express();
+
+const server = app.listen(3000 , ()=>{
+   console.log("Here we go we, are connected in port 3000 !") ;
+});
+
+// Static files
+
+app.use(express.static('public'));
+
+// Socket setup
+const io = socket(server);
+
+
+io.on('connection', (socket) => {
+    console.log('made socket connection', socket.id);
+
+    socket.on('chat', (data) => {
+        console.log(data);
+        io.sockets.emit('chat', data)
+    });
+});
+
+
+
+
+
