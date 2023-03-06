1b89c96b7840d124c7adb7c809b683e396dfca66
Ahmed hossam <you@example.com>
2023-03-06 15:27:55 +0200

    broadcast done <3

--- index.js
+++ index.js
@@ -1,27 +1,26 @@
 const express = require('express');
 const socket = require('socket.io');
 const http = require('http');
-
-
-
+const {Server} = require('socket.io')
 //App setup :D
 const app = express();
 
 const server = app.listen(3000 , ()=>{
-   console.log("Here we go we, are connected in port 3000 !") ;
+   console.log("Here we go, we are connected in port 3000 !") ;
 });
 
 // Static files
 
 app.use(express.static('public'));
 
+
 // Socket setup
-const io = socket(server);
+const io = new Server(server);
 
 
-io.on('connection', (socket) => {
+io.on('connection', (socket) => { // socket refer to socket instance and user
+
     console.log('made socket connection', socket.id);
-
     socket.on('chat', (data) => {
         console.log(data);
         io.sockets.emit('chat', data)
@@ -29,6 +28,7 @@ io.on('connection', (socket) => {
     socket.on('typing', (data)=>{
        socket.broadcast.emit('typing' , data);
     });
+
 });
 
 
